303ce6db41f232820c3261ecb9ad5a16dffa4bc2
xuanvu99 <61828496+xuanvu99@users.noreply.github.com>
2020-12-07 09:07:43 +0700

    abcd

--- quan_ly_ho_so_api/public/index.php
+++ quan_ly_ho_so_api/public/index.php
@@ -16,4 +16,4 @@ if(file_exists(__DIR__.'/../vendor/autoload.php')) {
     require_once __DIR__.'/../vendor/autoload.php';
 }
 
-require_once __DIR__.'/../bootstrap/app.php';ffdsdsfs
+require_once __DIR__.'/../bootstrap/app.php';
