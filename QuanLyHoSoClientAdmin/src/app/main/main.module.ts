72dbdda01c120a23ccb0b858b9e07677075802a7
xuanvu99 <61828496+xuanvu99@users.noreply.github.com>
2020-12-10 23:12:42 +0700

    Update quản lý lĩnh vực admin

--- QuanLyHoSoClientAdmin/src/app/main/main.module.ts
+++ QuanLyHoSoClientAdmin/src/app/main/main.module.ts
@@ -5,17 +5,19 @@ import { MainRoutingModule } from './main-routing.module';
 import {ShareModule} from "../share/share.module";
 import {HomeComponent} from "./home/home.component";
 import { FeildComponent } from './feild/feild.component';
+import {TableModule} from "primeng/table";
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    FeildComponent
+    FeildComponent,
   ],
   imports: [
     CommonModule,
     ShareModule,
     MainRoutingModule,
+    TableModule,
   ]
 })
 export class MainModule { }
