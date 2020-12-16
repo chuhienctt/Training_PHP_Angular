78b813b6b84c9f1d45a82f3e10c85f81ff0d5403
ndt-pro <toannguyen080199@gmail.com>
2020-12-16 09:20:19 +0700

    update

--- quan_ly_ho_so_api/public/index.php
+++ quan_ly_ho_so_api/public/index.php
@@ -5,7 +5,10 @@
  *
  * @package  NDT
  * @author   NDTpro <admin@ndtoan.com>
- */// autoload composer library
+ */
+
+ // autoload composer library
+
 if(file_exists(__DIR__.'/../vendor/autoload.php')) {
     require_once __DIR__.'/../vendor/autoload.php';
 }
