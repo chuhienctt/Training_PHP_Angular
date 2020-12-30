9ac7f7794c03fda677c0d88d8a1e8181b27f9e48
ndt-pro <toannguyen080199@gmail.com>
2020-12-30 10:08:18 +0700

    Fix get user, hidden special vars in model

--- quan_ly_ho_so_api/app/Models/Users.php
+++ quan_ly_ho_so_api/app/Models/Users.php
@@ -6,6 +6,9 @@ use Core\Model;
 
 class Users extends Model {
     protected $table = 'users';
+    protected $hidden = [
+        'mat_khau'
+    ];
 
     public function checkBlock() {
         return $this->deleted_at != NULL;
