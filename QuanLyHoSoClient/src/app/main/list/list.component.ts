f23d9b1ac5bf5b48518ad2366236fba593ebaf39
xuanvu99 <61828496+xuanvu99@users.noreply.github.com>
2021-01-22 15:04:59 +0700

    Update list page

--- QuanLyHoSoClient/src/app/main/list/list.component.ts
+++ QuanLyHoSoClient/src/app/main/list/list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injector, OnInit} from '@angular/core';
 import {ListService} from "../../services/list.service";
 import {ActivatedRoute, ParamMap} from "@angular/router";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ScriptService} from "../../../../../QuanLyHoSoClientAdmin/src/app/libs/script.service";
 
 @Component({
   selector: 'app-list',
@@ -26,7 +27,7 @@ export class ListComponent implements OnInit {
     private listService:ListService,
     private route: ActivatedRoute,
     private formBuilder: FormBuilder
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
@@ -62,22 +63,26 @@ export class ListComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.form.value)
     this.pageChange(this.page);
   }
 
   filterByOrgan(id_co_quan) {
-    this.form.value.id_co_quan = id_co_quan;
+    this.form.patchValue({
+      id_co_quan: String(id_co_quan)
+    })
     this.form.value.id_linh_vuc = "";
     this.pageChange(this.page);
   }
 
   filterByFeild(id_linh_vuc) {
-    this.form.value.id_linh_vuc = id_linh_vuc;
+    this.form.patchValue({
+      id_linh_vuc: id_linh_vuc
+    })
     this.pageChange(this.page);
   }
 
   getFeild(event) {
+    console.log(event.target.value)
     this.form.value.id_linh_vuc = "";
     if (!event.target.value) {
       this.getAllFeild();
